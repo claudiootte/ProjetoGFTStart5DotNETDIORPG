89dbaafb1dd3e33da97739973b8dd3a6a85aade6
Claudio Otte <cc.otte@hotmail.com>
2022-05-31 21:26:26 -0300

    Make the program functional and very organized

--- src/RPGCore/Program.cs
+++ src/RPGCore/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace DIORPG
+﻿using DIORPG.RPGCharacters;
+namespace DIORPG
 {
     class Program
     {
         static void Main()
         {
-
+            Hunter Dean = new Hunter("Dean Winchester");
+            Dean.Atack(AtackWith.Gun);
+            Console.WriteLine(Dean.Name);
 
         }
     }
